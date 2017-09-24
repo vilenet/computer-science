087b22475fd29f983c5aa93ba33cca64f08e7013
Viktor <vilenet607@gmail.com>
2017-09-24 10:16:53 +0300

    Update 2_Level_Order_Tree_Traversal_1.cpp

--- 2 DS/Binary tree/1 Traversals/2_Level_Order_Tree_Traversal_1.cpp
+++ 2 DS/Binary tree/1 Traversals/2_Level_Order_Tree_Traversal_1.cpp
@@ -16,34 +16,30 @@ struct Node* newNode(int newData){
   return node;
 }
 
-void printGivenLevel(struct Node* root, int level){
+// find the height of a binary tree
+int height(struct Node* root){
+   if(root == NULL)return 0;
+   else{
+	   int lh = height(root->left);
+	   int rh = height(root->right);
+	   if(lh > rh) return lh+1;
+	   else return rh + 1;
+   }
+}
+
+void printLevel(struct Node* root, int level){
   if (root == NULL) return;
 
   if(level == 1)
     cout << root->data << " ";
   else if(level > 1){
-    printGivenLevel(root->left, level-1);
-    printGivenLevel(root->right, level-1);
+    printLevel(root->left,  level-1);
+    printLevel(root->right, level-1);
   }
 }
 
-int getHeight(struct Node* node){
-  if (node==NULL)
-    return 0;
-  else{
-    int leftHeight  = getHeight(node->left);
-    int rightHeight = getHeight(node->right);
-
-    if(leftHeight > rightHeight)
-      return(leftHeight+1);
-    else
-      return(rightHeight+1);
-  }
-
-}
-
-void printLevelOrder(struct Node* root){
-    int h = getHeight(root);
+void printOrderTree(struct Node* root){
+    int h = height(root);
     for(int i=1; i<=h; ++i)
       printGivenLevel(root, i);
 }
@@ -57,7 +53,7 @@ int main()
   root->left->right = newNode(5);
 
   cout << "Level Order traversal of binary tree is \n";
-  printLevelOrder(root);
+  printOrderTree(root);
 
 }
 
