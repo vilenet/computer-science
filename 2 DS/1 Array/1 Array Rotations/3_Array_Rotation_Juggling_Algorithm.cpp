17b0efefcb1661ec75e901bde6a3b45215579240
Viktor <vilenet607@gmail.com>
2018-04-05 23:49:33 +0300

    Update 3_Array_Rotation_Juggling_Algorithm.cpp

--- 2 DS/1 Array/1 Array Rotations/3_Array_Rotation_Juggling_Algorithm.cpp
+++ 2 DS/1 Array/1 Array Rotations/3_Array_Rotation_Juggling_Algorithm.cpp
@@ -3,47 +3,37 @@
 using namespace std;
 
 void printArray(int arr[], int N){
-  for(int i = 0; i < N; ++i)
-    cout << arr[i] << " ";
-  cout << endl;
+    for(int i = 0; i < N; i++)
+      cout << arr[i] << " ";
+    cout << endl;  
+}
+ 
+int gcd(int a,int b){
+    if(b == 0) return a;
+    else       return gcd(b, a%b);
 }
 
-/*Fuction to get gcd of a and b*/
-int gcd(int a, int b){
-   if(b==0)
-     return a;
-   else
-     return gcd(b, a%b);
-}
-
-/*Function to left rotate arr[] of siz n by d*/
-void leftRotate(int arr[], int d, int n){
-
-  int i, j, k, temp;
-  for (i = 0; i < gcd(d, n); ++i){
-    /* move i-th values of blocks */
-    temp = arr[i];
-    j = i;
-    while(1){
-      k = j + d;
-      if (k >= n)
-        k = k - n;
-      if (k == i)
-        break;
-      arr[j] = arr[k];
-      j = k;
-    }
-    arr[j] = temp;
-  }
+void LeftRotate(int arr[], int d, int n){
+   for(int i = 0; i < gcd(d,n); ++i){
+     int temp = arr[i];
+     int j = i;
+     while(1){
+       int k = j+d;
+       if(k >= n) k = k-n;
+       if(k == i) break;
+       arr[j] = arr[k];
+       j = k;
+     }
+     arr[j] = temp;
+   }
 }
 
 int main()
 {
-   int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-   int N = sizeof(arr)/sizeof(arr[0]);
-   leftRotate(arr, 2, N);
-   printArray(arr, N);
-
-   return 0;
+  int arr[] = {1,2,3,4,5,6,7}; 
+  int N = sizeof(arr)/sizeof(arr[0]);	
+  LeftRotate(arr, 2, N);
+  printArray(arr, N);
+  
+  return 0;
 }
-
