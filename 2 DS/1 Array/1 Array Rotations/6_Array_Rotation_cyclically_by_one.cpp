f9ec05335297b3a90f26345f051ccc94884dc2c8
Viktor <vilenet607@gmail.com>
2018-04-09 07:35:16 +0300

    Update 6_Array_Rotation_cyclically_by_one.cpp

--- 2 DS/1 Array/1 Array Rotations/6_Array_Rotation_cyclically_by_one.cpp
+++ 2 DS/1 Array/1 Array Rotations/6_Array_Rotation_cyclically_by_one.cpp
@@ -1,7 +1,6 @@
 void rotate(int arr[], int n){
-	int temp = arr[n-1];
-	for(int i = n-1; i > 0; i--)
-	  arr[i] = arr[i-1];
-    arr[0] = temp;
+     int temp = arr[n-1];
+     for(int i = n-1; i > 0; i--)
+	arr[i] = arr[i-1];
+     arr[0] = temp;
 }	
-}
