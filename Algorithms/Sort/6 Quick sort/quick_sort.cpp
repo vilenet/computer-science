3a62bcfca02d839f02db1d0c961d591b823b0795
Viktor Zinoviev <vilenet607@gmail.com>
2024-07-19 13:52:54 +0100

    Added branches

--- Algorithms/Sort/6 Quick sort/quick_sort.cpp
+++ Algorithms/Sort/6 Quick sort/quick_sort.cpp
@@ -6,9 +6,11 @@ int partition(int arr[], int low, int high){
 	int pivot = arr[high];
 	int i = (low - 1);
 
-	for(int j = low; j <= high- 1; j++)
-      if(arr[j] <= pivot)
+	for(int j = low; j <= high- 1; j++){
+      if(arr[j] <= pivot){
         std::swap(arr[++i], arr[j]);
+	  }
+	}
 
 	std::swap(arr[i + 1], arr[high]);
 	return (i + 1);
