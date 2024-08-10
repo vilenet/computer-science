4d1de41cfc3307665a87b0406e4336103a5f53b0
Viktor Zinoviev <vilenet607@gmail.com>
2024-08-10 16:33:23 +0100

    add operators + - getPtr, usings

--- Structures/vector/my_vector.cpp
+++ Structures/vector/my_vector.cpp
@@ -1,6 +1,9 @@
 // Basic vector implementation
 #include <iostream>
-#include <cstdlib>
+#include <iterator>
+#include <stdexcept> 
+#include <algorithm>
+#include <initializer_list>
 
 namespace mystd
 {
@@ -31,6 +34,14 @@ public:
         T* m_ptr;
 
     public:
+        // Necessary typedefs for iterator properties
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = T*;
+        using reference = T&;
+
+    public:
         Iterator(int* ptr) : m_ptr(ptr) {}
 
         T& operator*() const { return *m_ptr; }
@@ -48,13 +59,48 @@ public:
 
         bool operator==(const Iterator& other) const { return m_ptr == other.m_ptr; }
         bool operator!=(const Iterator& other) const { return m_ptr != other.m_ptr; }
+
+        Iterator operator+(size_t n) const { return Iterator(m_ptr + n); }
+        Iterator operator-(size_t n) const { return Iterator(m_ptr - n); }
+
+        ptrdiff_t operator-(const Iterator& other) const { return m_ptr - other.m_ptr; }
+
+        T* getPtr() const { return m_ptr; }
     };
 
 public:
     vector() : m_data(nullptr), m_size(0), m_capacity(0) {}
-    //TODO: Ct with args
+
+    vector(const T& value) : m_data(new T[1]), m_size(1), m_capacity(1) {
+        m_data[0] = value;
+    }
+
+    vector(std::initializer_list<T> initList)
+        : m_data(new T[initList.size()])
+        , m_size(initList.size())
+        , m_capacity(initList.size()) 
+    {
+        std::copy(initList.begin(), initList.end(), m_data);
+    }
+
     ~vector() { delete[] m_data; }
-    //TODO: operator=
+
+
+    vector& operator=(const vector& other) {
+        if (this != & other) {
+            delete[] m_data;
+        }
+
+        m_size = other.m_size;
+        m_capacity = other.m_capacity;
+        m_data = new T[m_capacity];
+
+        for (size_t i = 0; i < m_size; ++i) {
+            m_data[i] = other.m_data[i];
+        }
+
+        return *this;
+    }
 
     // Iterators methods:
     Iterator begin() { return Iterator(m_data); }
@@ -85,8 +131,31 @@ public:
         m_data[m_size++] = value;
     }
 
-    //TODO: erase()
+    Iterator erase(Iterator first, Iterator last) {
+        if (first.getPtr() < m_data || 
+            first.getPtr() > m_data + m_size || 
+            last.getPtr() < m_data || 
+            last.getPtr() > m_data + m_size) 
+        {
+            throw std::out_of_range("Iterator out of range");
+        }
 
+        T* start = first.getPtr();
+        T* end = last.getPtr();
+
+        size_t num_elements_to_remove = end - start;
+
+        for (T* it = start; it != m_data + m_size - num_elements_to_remove; ++it) {
+            *it = *(it + num_elements_to_remove);
+        }
+
+        m_size -= num_elements_to_remove;
+        return Iterator(start);
+    }
+
+    Iterator erase(Iterator pos) {
+        return erase(pos, Iterator(pos.getPtr() + 1));
+    }
 };
 
 } // End of mystd namespace
@@ -95,15 +164,24 @@ public:
 int main()
 {
     mystd::vector<int> vec1;
+    mystd::vector<int> vec2(15);
+    mystd::vector<int> vec3;
+    mystd::vector<int> vec4 = {1, 2, 3, 4, 5, 3, 6, 3, 7};
 
     vec1.push_back(10);
     vec1.push_back(20);
     vec1.push_back(30);
 
-    // C++11
-    // for (auto n : vec1) {
-    //     std::cout << n << " ";
-    // } 
+    
+    for (int i=1; i<=10; i++) vec3.push_back(i);
+
+    // erase the 6th element
+    vec3.erase(vec3.begin() + 5);
+
+    // erase the first 3 elements:
+    vec3.erase(vec3.begin(), vec3.begin() + 3);
+
+    vec1.erase(std::remove(vec1.begin(), vec1.end(), 20), vec1.end());
 
     // C++98
     for (mystd::vector<int>::Iterator it = vec1.begin(); it != vec1.end(); ++it) {
@@ -111,6 +189,11 @@ int main()
     }
 
     std::cout << std::endl;
+
+    // C++11
+    for (auto n : vec3) {
+        std::cout << n << " ";
+    } 
 
     return 0;
 }
