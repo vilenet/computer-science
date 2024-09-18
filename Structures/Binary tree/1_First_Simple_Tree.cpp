159a8c9a961367922add495bcd9c1e30d1c49ee3
Viktor Zinoviev <vilenet607@gmail.com>
2024-09-18 10:34:53 +0100

    fixed simple tree example

--- Structures/Binary tree/1_First_Simple_Tree.cpp
+++ Structures/Binary tree/1_First_Simple_Tree.cpp
@@ -2,26 +2,41 @@
 using namespace std;
 
 struct Node{
-  int data;
-  struct Node* left;
-  struct Node* right;
+    int data;
+    Node* left = nullptr;
+    Node* right = nullptr;
 };
 
-struct Node* newNode(int newData){
-  struct Node* node = new struct Node;
-  node->data = newData;
-  node->left = NULL;
-  node->right = NULL;
-  return
+Node* newNode(int newData){
+    Node* node = new Node;
+    node->data = newData;
+    return node;
+}
+
+void deleteTree(Node* node) {
+    if (node == nullptr) { return; }
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}
+
+void inorderTraversal(Node* node) {
+    if (node == nullptr) { return; }
+    inorderTraversal(node->left);
+    std::cout << node->data << " ";
+    inorderTraversal(node->right);
 }
 
 int main()
 {
-  struct Node* root = newNode(1);
-  root->left        = newNode(2);
-  root->right       = newNode(3);
-  root->left->left  = newNode(4);
-  root->left->right = newNode(5);
-  
+    Node* root        = newNode(1);
+    root->left        = newNode(2);
+    root->right       = newNode(3);
+    root->left->left  = newNode(4);
+    root->left->right = newNode(5);
+    std::cout << "Inorder traversal: ";
+    inorderTraversal(root);
+    deleteTree(root);
+
+    return 0;
 }
-    
