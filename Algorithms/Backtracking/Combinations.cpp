08a8e0fde0ec96730b8465eca3f1e40d4bee8060
Viktor Zinoviev <vilenet607@gmail.com>
2025-01-05 22:16:35 +0000

    upd combinations

--- Algorithms/Backtracking/Combinations.cpp
+++ Algorithms/Backtracking/Combinations.cpp
@@ -1,49 +1,37 @@
+// Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
 #include <iostream>
 #include <vector>
 using namespace std;
 
 
-class Solution {
-public:
-    // Метод для поиска комбинаций с использованием backtracking
-    void backtrack(int start, int n, int k, vector<int>& combination, vector<vector<int>>& result) {
-        // Если комбинация собрана, добавляем её в результат
-        if (combination.size() == k) {
-            result.push_back(combination);
-            return;
-        }
-
-        // Перебираем числа от текущего `start` до `n`
-        for (int i = start; i <= n; ++i) {
-            // Добавляем число в текущую комбинацию
-            combination.push_back(i);
-
-            // Рекурсивно продолжаем собирать комбинацию
-            backtrack(i + 1, n, k, combination, result);
-
-            // Возврат к предыдущему состоянию (убираем последнее число)
-            combination.pop_back();
-        }
+// Метод для поиска комбинаций с использованием backtracking
+void backtrack(int start, int n, int k, vector<int>& combination, vector<vector<int>>& result) {
+    
+    if (combination.size() == k) {                   // Если комбинация собрана, добавляем её в результат
+        result.push_back(combination);
+        return;
     }
-
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> result;
-        vector<int> combination;
-
-        // Запуск backtracking с начальным индексом 1
-        backtrack(1, n, k, combination, result);
-
-        return result;
+         
+    for (int i = start; i <= n; ++i) {               // Перебираем числа от текущего `start` до `n`
+        combination.push_back(i);                    // Добавляем число в текущую комбинацию
+        backtrack(i + 1, n, k, combination, result); // Рекурсивно продолжаем собирать комбинацию
+        combination.pop_back();                      // Возврат к предыдущему состоянию (убираем последнее число)
     }
-};
+}
+
+vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> result;
+    vector<int> combination;
+    backtrack(1, n, k, combination, result);         // Запуск backtracking с начальным индексом 1
+    return result;
+}
 
 
 int main() 
 {
-    Solution solution;
     int n = 4, k = 2;
 
-    vector<vector<int>> combinations = solution.combine(n, k);
+    vector<vector<int>> combinations = combine(n, k);
 
     for (const auto& comb : combinations) {
         for (int num : comb) {
