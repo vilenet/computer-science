5a30dce2708fd9871fa78404d338d4402e7d556c
Viktor Zinoviev <vilenet607@gmail.com>
2025-01-15 20:38:22 +0000

    Upd Combinations.cpp

--- Algorithms/Backtracking/Combinations.cpp
+++ Algorithms/Backtracking/Combinations.cpp
@@ -4,25 +4,24 @@
 using namespace std;
 
 
-// Метод для поиска комбинаций с использованием backtracking
 void backtrack(int start, int n, int k, vector<int>& combination, vector<vector<int>>& result) {
-    
-    if (combination.size() == k) {                   // Если комбинация собрана, добавляем её в результат
+    if (combination.size() == k) {                   // If the combination is complete, add it to the result
         result.push_back(combination);
         return;
     }
          
-    for (int i = start; i <= n; ++i) {               // Перебираем числа от текущего `start` до `n`
-        combination.push_back(i);                    // Добавляем число в текущую комбинацию
-        backtrack(i + 1, n, k, combination, result); // Рекурсивно продолжаем собирать комбинацию
-        combination.pop_back();                      // Возврат к предыдущему состоянию (убираем последнее число)
+    for (int i = start; i <= n; ++i) {               // Iterate through numbers from the current `start` to `n`
+        combination.push_back(i);                    // Add the number to the current combination
+        backtrack(i + 1, n, k, combination, result); // Recursively continue to build the combination
+        combination.pop_back();                      // Backtrack to the previous state (remove the last number)
     }
 }
 
 vector<vector<int>> combine(int n, int k) {
     vector<vector<int>> result;
     vector<int> combination;
-    backtrack(1, n, k, combination, result);         // Запуск backtracking с начальным индексом 1
+    result.reserve(1 << n);                          // Reserve space to avoid multiple allocations
+    backtrack(1, n, k, combination, result);         // Start backtracking with the initial index 1
     return result;
 }
 
